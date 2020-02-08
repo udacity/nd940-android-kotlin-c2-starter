b46439118cf52dec9e7b94a1a421201fe92301b3
Jesus Almaral <almaral17@gmail.com>
2020-02-08 14:30:15 -0700

    Display explanation dialog when tapping help button

--- starter/app/src/main/java/com/udacity/asteroidradar/detail/DetailFragment.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/detail/DetailFragment.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import com.udacity.asteroidradar.R
 import com.udacity.asteroidradar.databinding.FragmentDetailBinding
 
 class DetailFragment : Fragment() {
@@ -18,6 +20,17 @@ class DetailFragment : Fragment() {
 
         binding.asteroid = asteroid
 
+        binding.helpButton.setOnClickListener {
+            displayAstronomicalUnitExplanationDialog()
+        }
+
         return binding.root
+    }
+
+    private fun displayAstronomicalUnitExplanationDialog() {
+        val builder = AlertDialog.Builder(activity!!)
+            .setMessage(getString(R.string.astronomica_unit_explanation))
+            .setPositiveButton(android.R.string.ok, null)
+        builder.create().show()
     }
 }
