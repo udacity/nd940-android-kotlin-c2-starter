0cce8a718300c69402a7fb91f5f2f652b5389088
Archangel354 <rduncan354@gmail.com>
2022-03-05 14:34:20 -0500

    in MainViewModel.kt,  I changed response and _response to status _status.  I also added _property and get() = _property. In fragment_main.xml,  I changed the livedata for text in textView2 from @{viewModel.response} to @{viewModel.status}

--- starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -4,17 +4,23 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.udacity.asteroidradar.PictureOfDay
 import com.udacity.asteroidradar.api.PictureApi
 import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
 
     // The internal MutableLiveData String that stores the status of the most recent request
-    private val _response = MutableLiveData<String>()
+    private val _status = MutableLiveData<String>()
 
     // The external immutable LiveData for the request status String
-    val response: LiveData<String>
-        get() = _response
+    val status: LiveData<String>
+        get() = _status
+
+    private val _property = MutableLiveData<PictureOfDay>()
+
+    val property: LiveData<PictureOfDay>
+        get() = _property
 
     /**
      * Call getPictureProperties() on init so we can display status immediately.
@@ -31,9 +37,9 @@ class MainViewModel : ViewModel() {
             try{
                 var listResult =
                     PictureApi.retrofitService.getProperties()
-                _response.value = "Success: ${listResult} Picture properties retrieved"
+                _status.value = "Success: ${listResult} Picture properties retrieved"
             }catch (e: Exception) {
-                _response.value = "Failure: ${e.message}"
+                _status.value = "Failure: ${e.message}"
             }
 
         }
