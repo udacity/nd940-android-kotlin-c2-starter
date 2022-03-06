1a66ce742c5b96b44f3a8670db09dda3126768f5
Archangel354 <rduncan354@gmail.com>
2022-03-06 17:43:57 -0500

    in MainViewModel.kt,  I create the variable urstring which I save the parsed JSON string to.   I parse the string down to the jpg file which can be display.

--- starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -1,9 +1,12 @@
 package com.udacity.asteroidradar.main
 
+import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.udacity.asteroidradar.MainActivity
 import com.udacity.asteroidradar.PictureOfDay
 import com.udacity.asteroidradar.api.PictureApi
 import kotlinx.coroutines.launch
@@ -35,12 +38,36 @@ class MainViewModel : ViewModel() {
     private fun getPictureProperties() {
         viewModelScope.launch {
             try{
-                var listResult =
-                    PictureApi.retrofitService.getProperties()
-                _status.value = "Success: ${listResult} Picture properties retrieved"
+
+                var listResult = PictureApi.retrofitService.getProperties()
+                var urlString: String = ""
+                //_status.value = "Success: ${listResult} Picture properties retrieved"
+                val responseList: List<String> = listResult.split("url")
+                //var testString = "0123456789"
+                Log.i("The list is",responseList.toString())
+
+                var i = 0
+                responseList.forEach {
+                   // Log.i("The foreach is\n",responseList[i].toString() + '\n')
+                    if (responseList[i].toString().contains("https")){
+                        urlString = responseList[i]
+                        urlString = urlString.substring(startIndex = 3)
+                        urlString = urlString.substringBefore("jpg")
+                        urlString = urlString + "jpg"
+                        //Log.i("inside foreach", testString.substring(startIndex = 2)+ '\n')
+                        Log.i("inside foreach", urlString+ '\n')
+
+                    }
+                    i++
+                }
+                _status.value = "Success: ${urlString} Picture properties retrieved"
+
+
+                Log.i("url is",responseList[1])
             }catch (e: Exception) {
                 _status.value = "Failure: ${e.message}"
             }
+
 
         }
     }
