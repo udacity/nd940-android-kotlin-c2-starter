a4d8172eed03e061cf63f4af544bf5bc2d5bec19
Jonathan Sueck <jonathansueck@gmail.com>
2022-03-16 22:26:02 -0400

    Set up the network, and ready to display the url and list of asteroids from the API. created variables to keep track of the parts of the URL and the API key. Added a Asteroid entity to create a database when the asteroids are loaded

--- starter/app/src/main/java/com/udacity/asteroidradar/api/AsteroidApiService.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/api/AsteroidApiService.kt
@@ -1,11 +1,27 @@
 package com.udacity.asteroidradar.api
 
-import retrofit2.Call
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.udacity.asteroidradar.Asteroid
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 
-private const val BASE_URL = "https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=AuLTKnSJnJ6atbnq6Yz4bIh4mnDbclmtCJiamhvK"
+private const val BASE_URL = "https://api.nasa.gov/neo/rest/v1/"
+private val api_key = "AuLTKnSJnJ6atbnq6Yz4bIh4mnDbclmtCJiamhvK"
+
+private val start_date = "2015-09-07"
+private val end_date = "2015-09-08"
+
+val url = "${BASE_URL}feed?start_date=${start_date}&end_date=${end_date}&api_key=${api_key}/"
+private val img_url = "https://api.nasa.gov/planetary/apod?api_key=${api_key}"
+//feed?start_date=2015-09-07&end_date=2015-09-08&api_key=AuLTKnSJnJ6atbnq6Yz4bIh4mnDbclmtCJiamhvK/
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+
 private val retrofit = Retrofit.Builder()
     .addConverterFactory(ScalarsConverterFactory.create())
     .baseUrl(BASE_URL)
@@ -14,7 +30,7 @@ private val retrofit = Retrofit.Builder()
 interface AsteroidApiService {
     @GET("asteroids")
     fun getAsteroids():
-            Call<String>
+            List<Asteroid>
 }
 
 object AsteroidApi {
--- starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -3,7 +3,11 @@ package com.udacity.asteroidradar.main
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.udacity.asteroidradar.Asteroid
 import com.udacity.asteroidradar.api.AsteroidApi
+import com.udacity.asteroidradar.api.AsteroidApi.retrofitService
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Response
 import retrofit2.Callback
@@ -14,25 +18,47 @@ class MainViewModel : ViewModel() {
 
     private val _status = MutableLiveData<AsteroidApiStatus>()
 
+    val status: LiveData<AsteroidApiStatus>
+        get() = _status
+
     private val _response = MutableLiveData<String>()
 
     val response: LiveData<String>
         get() = _response
 
+    private val _asteroids = MutableLiveData<List<Asteroid>>()
 
-    init{
+    val asteroids: LiveData<List<Asteroid>>
+        get() = _asteroids
 
+    private val _navigateToSelectedAsteroid = MutableLiveData<Asteroid>()
+
+    val navigateToSelectedProperty: LiveData<Asteroid>
+        get() = _navigateToSelectedAsteroid
+
+    init {
+        getAsteroids()
     }
 
     private fun getAsteroids() {
-    AsteroidApi.retrofitService.getAsteroids().enqueue(object : Callback<String> {
-        override fun onFailure(call: Call<String>, t: Throwable) {
-            _response.value = "Failure: " + t.message
+
+        viewModelScope.launch {
+            _status.value = AsteroidApiStatus.LOADING
+            try {
+                _asteroids.value = AsteroidApi.retrofitService.getAsteroids()
+                _status.value = AsteroidApiStatus.DONE
+            } catch (e: Exception) {
+                _status.value = AsteroidApiStatus.ERROR
+                _asteroids.value = ArrayList()
+            }
+        }
         }
 
-        override fun onResponse(call: Call<String>, response: Response<String>) {
-            _response.value = response.body()
-        }
-    })
-}
+    fun displayAsteroidDetail(asteroid: Asteroid) {
+        _navigateToSelectedAsteroid.value = asteroid
+    }
+
+    fun displayAsteroidDetailsComplete() {
+        _navigateToSelectedAsteroid.value = null
+    }
 }
