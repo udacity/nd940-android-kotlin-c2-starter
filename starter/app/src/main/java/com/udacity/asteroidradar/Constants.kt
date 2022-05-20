23559d203fa6430450a42cb3f7b822251bf5ff2f
Jonathan Sueck <jonathansueck@gmail.com>
2022-05-20 17:20:33 -0400

    Added @Query annotations to pass the dynamic date string to retrofit, and also the api key.

--- starter/app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -1,7 +1,10 @@
 package com.udacity.asteroidradar
 
+
 object Constants {
     const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
-    const val BASE_URL = "https://api.nasa.gov/"
+    const val BASE_URL = "https://api.nasa.gov/neo/rest/v1/"
+    const val START_DATE = "start_date"
+    const val API_KEY = "api_key"
 }
--- starter/app/src/main/java/com/udacity/asteroidradar/api/AsteroidApiService.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/api/AsteroidApiService.kt
@@ -2,31 +2,32 @@ package com.udacity.asteroidradar.api
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import com.udacity.asteroidradar.Asteroid
+import com.udacity.asteroidradar.Constants
 import com.udacity.asteroidradar.PictureOfDay
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Query
 
-private const val base_url = "https://api.nasa.gov/neo/rest/v1/"
+private const val base_url = Constants.BASE_URL
 private const val api_key = "AuLTKnSJnJ6atbnq6Yz4bIh4mnDbclmtCJiamhvK"
-
-private const val start_date = "2015-09-13"
-private val end_date = "2015-09-08"
-
-const val url = "${base_url}feed?start_date=2015-09-07&end_date=2015-09-13&api_key=AuLTKnSJnJ6atbnq6Yz4bIh4mnDbclmtCJiamhvK/"
 const val img_url = "https://api.nasa.gov/planetary/apod?api_key=${api_key}"
 
 interface AsteroidApiService {
-    // todo  Change this to add the start date as a passed parameter
-    @GET("feed?start_date=2022-05-13&api_key=AuLTKnSJnJ6atbnq6Yz4bIh4mnDbclmtCJiamhvK")
-    suspend fun getAsteroids():
-            String
 
     @GET(img_url)
     suspend fun getImageOfTheDay():
             PictureOfDay
+
+    @GET("feed?")
+    suspend fun getAsteroids(
+        // Queries annotations to pass the dynamic date & API key to retrofit
+        @Query(Constants.START_DATE) todayDate:String = getDate(),
+        @Query(Constants.API_KEY) apiKey:String = api_key
+    ): String
+
+
 }
 
 private val moshi = Moshi.Builder()
