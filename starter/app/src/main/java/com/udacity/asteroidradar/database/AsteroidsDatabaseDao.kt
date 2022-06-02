a0d6ca2794aba06f17216b087f2a0a4335eeeb88
Jonathan Sueck <jonathansueck@gmail.com>
2022-06-02 16:57:43 -0400

    changed the getAll() function to only get asteroids that have a close appooch date greater or equal to todays date, need to change this from a string to a dynamic value

--- starter/app/src/main/java/com/udacity/asteroidradar/database/AsteroidsDatabaseDao.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/database/AsteroidsDatabaseDao.kt
@@ -12,8 +12,11 @@ interface AsteroidsDatabaseDao {
         @Insert(onConflict = OnConflictStrategy.REPLACE)
         fun insert(asteroid: DatabaseAsteroid)
 
-        @Query("SELECT * FROM asteroids_database ORDER BY close_approach_date DESC")
+        @Query("SELECT * FROM asteroids_database WHERE close_approach_date >= '2022-06-01'")
         suspend fun getAll(): List<DatabaseAsteroid>
+
+        @Query("SELECT * FROM asteroids_database WHERE close_approach_date is '2022-06-02'")
+        suspend fun getTonight(): List<DatabaseAsteroid>
 
         @Query("SELECT * from asteroids_database WHERE id = :id")
         fun getAsteroidById(id: Long): LiveData<DatabaseAsteroid>
