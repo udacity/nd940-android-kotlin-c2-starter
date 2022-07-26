e41e391640fdc362ac218ebcabb414c0f449b546
George Nady <gorgonady.gn@gmail.com>
2022-07-26 02:04:27 +0200

    setup material design 3 theme

--- starter/app/src/main/java/com/udacity/asteroidradar/Asteroid.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/Asteroid.kt
@@ -4,7 +4,9 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class Asteroid(val id: Long, val codename: String, val closeApproachDate: String,
-                    val absoluteMagnitude: Double, val estimatedDiameter: Double,
-                    val relativeVelocity: Double, val distanceFromEarth: Double,
-                    val isPotentiallyHazardous: Boolean) : Parcelable
+data class Asteroid(
+    val id: Long, val codename: String, val closeApproachDate: String,
+    val absoluteMagnitude: Double, val estimatedDiameter: Double,
+    val relativeVelocity: Double, val distanceFromEarth: Double,
+    val isPotentiallyHazardous: Boolean
+) : Parcelable
