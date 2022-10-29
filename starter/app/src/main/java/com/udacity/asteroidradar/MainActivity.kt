b38284d80609cb3d0323489c01b764c66006a377
EsraaAkram <totalappsingplay@gmail.com>
2022-10-29 14:36:13 +0200

    Update gradle and libraries

--- starter/app/src/main/java/com/udacity/asteroidradar/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/MainActivity.kt
@@ -8,5 +8,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //TODO: IN CASE THE PROJECT DID NOT FIND THE ANDROID_HOME
+        //ADD YOUR SDK PATH TO local.properties
+        //sdk.dir=F\:\\AllSDKs\\sdk_win
     }
 }
