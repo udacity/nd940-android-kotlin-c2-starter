508b847d730aca4e3b7ab39cec37dab5f42f6a9a
hieppham1999 <hieppham1999@gmail.com>
2024-03-31 21:09:47 +0700

    implement worker

---
+++ starter/app/src/main/java/com/udacity/asteroidradar/AsteroidRadarApplication.kt
@@ -0,0 +1,47 @@
+package com.udacity.asteroidradar
+
+import android.app.Application
+import androidx.work.Constraints
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.NetworkType
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
+import com.udacity.asteroidradar.work.RefreshDataWorker
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.concurrent.TimeUnit
+
+class AsteroidRadarApplication : Application() {
+
+    private val applicationScope = CoroutineScope(Dispatchers.Default)
+    override fun onCreate() {
+        super.onCreate()
+        delayedInit()
+    }
+
+    private fun delayedInit() {
+        applicationScope.launch {
+            setupRecurringWork()
+        }
+    }
+
+    private fun setupRecurringWork() {
+
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.UNMETERED)
+            .setRequiresBatteryNotLow(true)
+            .setRequiresCharging(true)
+            .build()
+
+        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(
+            1, TimeUnit.DAYS
+        ).setConstraints(constraints).build()
+
+        WorkManager.getInstance().enqueueUniquePeriodicWork(
+            RefreshDataWorker.WORK_NAME,
+            ExistingPeriodicWorkPolicy.KEEP,
+            repeatingRequest
+        )
+    }
+}
---
+++ starter/app/src/main/java/com/udacity/asteroidradar/work/RefreshDataWorker.kt
@@ -0,0 +1,27 @@
+package com.udacity.asteroidradar.work
+
+import android.content.Context
+import android.content.ContextParams
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.udacity.asteroidradar.database.getDatabase
+import com.udacity.asteroidradar.repository.AsteroidRepository
+import retrofit2.HttpException
+
+class RefreshDataWorker(appContext: Context, params: WorkerParameters): CoroutineWorker(appContext, params) {
+
+    companion object {
+        const val WORK_NAME = "AsteroidRefreshDataWorker"
+    }
+    override suspend fun doWork(): Result {
+        val database = getDatabase(applicationContext)
+        val repository = AsteroidRepository(database)
+
+        return try {
+            repository.refreshAsteroids()
+            return Result.success()
+        } catch (e: HttpException) {
+            return Result.failure()
+        }
+    }
+}
